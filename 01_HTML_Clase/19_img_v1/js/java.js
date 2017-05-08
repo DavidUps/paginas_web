b9cbc2abd161ce2cc79c585d8b0fe02c8ea5dae8
davidups <davidarribas.ciclos@gmail.com>
2017-05-08 14:29:01 +0200

    java

--- 01_HTML_Clase/19_img_v1/js/java.js
+++
@@ -1,7 +0,0 @@
-function manosTodo(){
-    var im, with,height;
-    im = document.getElementById("masTodo");
-    with = im.with;
-    height = im.height;
-    alert("Res: " with + height);
-}
---
+++ 06_HTML_Clase/19_img_v1/js/java.js
@@ -0,0 +1,19 @@
+function masTodo(){
+    document.getElementById("img1").
+    a.width+="10px";
+    document.getElementById("img2").style.width+="10px";
+    document.getElementById("img3").style.width+="10px";
+
+}
+
+function igualTodo(){
+	document.getElementById("img1").style.width+="100px";
+    document.getElementById("img2").style.width+="100px";
+    document.getElementById("img3").style.width+="100px";
+}
+
+function menosTodo(){
+	document.getElementById("img1").style.width+="-10px";
+	document.getElementById("img2").style.width+="-10px";
+	document.getElementById("img3").style.width+="-10px";
+}
